72adc88e069bf4ee0fe388c2843ca43d78856978
Nick Cosby <nickccosby@gmail.com>
2018-05-21 12:33:06 -0700

    Added config for routes

--- SmartTourist/src/app/app.module.ts
+++ SmartTourist/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +17,8 @@ import { UserService } from './user.service';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [PlacesService, UserService],
   bootstrap: [AppComponent]

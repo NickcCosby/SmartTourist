d420fed8dc5afd8c66eedc52d02a6804c91aa9ec
Ian_Danner <norseman117@gmail.com>
2018-05-22 11:40:10 -0700

    minor changes

--- SmartTourist/src/app/login/login.component.ts
+++ SmartTourist/src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user.service';
 
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -11,31 +12,8 @@ export class LoginComponent implements OnInit {
   constructor(public auth: UserService) { }
 
   ngOnInit() {
+
   }
-
-  // login() {
-  //   console.log("you clicked the log in button!")
-  //   this.auth.login();
-  // }
-
-  // location="";
-
-  // getLocation() {
-  //   function showPosition(position) {
-  //     console.log("HERE")
-  //     this.location = "Latitude: " + position.coords.latitude + 
-  //     "<br>Longitude: " + position.coords.longitude;
-  //   }
-
-  //   if (navigator.geolocation) {
-  //     console.log("WHAT")
-  //     console.log(navigator.geolocation.getCurrentPosition);
-  //   } else { 
-  //     this.location = "Geolocation is not supported by this browser.";
-  //   }
-
-  //   console.log("?")
-  // }
 
   
 }

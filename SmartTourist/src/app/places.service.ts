3558975c6fe681057f1352b6498747d37cbb1021
Nick Cosby <nickccosby@gmail.com>
2018-05-22 12:59:45 -0700

    Changed formatting of places to be an array of arrays as opposed to an object of arrays

--- SmartTourist/src/app/places.service.ts
+++ SmartTourist/src/app/places.service.ts
@@ -34,7 +34,7 @@ export class PlacesService {
       pagetoken:undefined,
       type:whitelist[0]
     };
-    var all = {};
+    var all = [];
     var callCount = 0;
     var errGot = false;
     try{
@@ -60,7 +60,7 @@ export class PlacesService {
               }
               data[iii].distance = this.getDistance([{lat:this.location.coords.latitude, long:this.location.coords.longitude}, {lat: data[iii].geometry.location.lat, long: data[iii].geometry.location.lng}]);
             }  
-            all[data[0].types[0]] = data;
+            all.push(data);
             callCount++
          }
         }).catch((err)=>{console.log(err); errGot = true;});

b41aae80e3fc41b45fe7a383301ffdb76bec24a4
Nick Cosby <nickccosby@gmail.com>
2018-05-23 10:06:44 -0700

    Started implementation of AGM

--- SmartTourist/src/app/app.module.ts
+++ SmartTourist/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { AgmCoreModule } from '@agm/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,7 +21,8 @@ import { CallbackComponent } from './callback/callback.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    AgmCoreModule.forRoot({apiKey:'AIzaSyAGIkqbumDrmgh73oEcNKovzAzOYQnqjuM'})
   ],
   providers: [PlacesService, UserService],
   bootstrap: [AppComponent]
